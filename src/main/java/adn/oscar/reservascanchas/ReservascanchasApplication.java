6b7dfb6ad6066793ed6171e492e90a2bd1ddd179
oscar.ruiz <oscar.ruiz@ceiba.com.co>
2021-01-14 22:03:40 -0500

    Debug Security Hotspots

--- src/main/java/adn/oscar/reservascanchas/ReservascanchasApplication.java
+++ src/main/java/adn/oscar/reservascanchas/ReservascanchasApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReservascanchasApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReservascanchasApplication.class, args);
+        SpringApplication.run(ReservascanchasApplication.class);
     }
 
 }
